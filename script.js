b369eb7882b192c1c132dc4e8acf8d6745c0458e
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-11-11 16:57:28 +0300

    inheritance between 'classes': constructor functions

--- script.js
+++ script.js
@@ -185,3 +185,42 @@ const sarah = Object.create(PersonProto);
 sarah.init("Sarah", 1979);
 sarah.calcAge();
 */
+
+// INHERITANCE BETWEEN "CLASSES": CONSTRUCTOR FUNCTIONS
+/*
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear); // Inherit properties
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student("Mike", 2020, "Computer Science");
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student); // true
+console.log(mike instanceof Person); // true
+console.log(mike instanceof Object); // true
+
+Student.prototype.constructor = Student; // Fixing the constructor pointer
+console.dir(Student.prototype.constructor);
+*/
