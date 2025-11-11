befd3441bed534ac0e7ef0717ba3556a7d7fa10d
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-11-11 13:40:56 +0300

    ES6 classes

--- script.js
+++ script.js
@@ -77,3 +77,35 @@ console.log(arr.unique());
 const h1 = document.querySelector("h1");
 console.dir((x) => x + 1);
 */
+
+// ES6 CLASSES
+/*
+// class expression
+// const PersonCl =class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1990);
+console.log(jessica);
+jessica.calcAge();
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+jessica.greet();
+
+// 1. classes are NOT hoisted
+// 2. classes are first-class citizens
+// 3. classes are executed in strict mode
+*/
